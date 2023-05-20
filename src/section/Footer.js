b4885ce7dabab3147654f77ed10229b9af777617
MetaFomos <dev.metafomos@gmail.com>
2023-05-20 15:26:37 +0900

    change footer links

--- src/section/Footer.js
+++ src/section/Footer.js
@@ -33,10 +33,20 @@ const Footer = () => {
               <div className="text-white">
                 <p className="text-[18px] md:text-[25px] text-left">Legal</p>
                 <p className="text-[14px] md:text-[21px] text-left lg:mt-[30px] mt-[15px]">
-                  Privacy Policy
+                  <a
+                    href="https://drive.google.com/file/d/1B1QH5JXoc0IopumgFRg3y1Qmqzytb1Hi/view?usp=share_link"
+                    target="_blank"
+                  >
+                    Privacy Policy
+                  </a>
                 </p>
                 <p className="text-[14px] md:text-[21px] text-left lg:mt-[30px] mt-[15px]">
-                  Terms Of Service
+                  <a
+                    href="https://drive.google.com/file/d/1oe8A9ZmLEjM27K_WydTxdbvlL3zCFvBA/view?usp=share_link"
+                    target="_blank"
+                  >
+                    Terms Of Service
+                  </a>
                 </p>
               </div>
             </div>
@@ -45,40 +55,59 @@ const Footer = () => {
                 Find Us Online
               </p>
               <div className="flex gap-5 mt-[30px]">
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/instagram.png"
-                />
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/linkedin.png"
-                />
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/facebook.png"
-                />
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/twitter.png"
-                />
+                <a
+                  href="https://www.instagram.com/dragonai_io/"
+                  target="_blank"
+                >
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/instagram.png"
+                  />
+                </a>
+                <a href="" target="_blank">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/linkedin.png"
+                  />
+                </a>
+                <a href="https://www.facebook.com/dragonaiio" target="_blank">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/facebook.png"
+                  />
+                </a>
+                <a href="https://twitter.com/DRAGONAI_IO" target="_blank">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/twitter.png"
+                  />
+                </a>
               </div>
               <div className="flex gap-5 my-3">
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/youtube.png"
-                />
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/telegram.png"
-                />
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/discord.png"
-                />
-                <img
-                  className="w-[30px] md:w-[50px]"
-                  src="/images/socials/email.png"
-                />
+                <a href="https://www.youtube.com/@DRAGONAI-IO" target="_blank">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/youtube.png"
+                  />
+                </a>
+                <a href="https://t.me/dragonai_io" target="_blank">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/telegram.png"
+                  />
+                </a>
+                <a href="https://discord.gg/4GZ9uvmsFh" target="_blank">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/discord.png"
+                  />
+                </a>
+                <a href="mailto:contact@dragonai.io">
+                  <img
+                    className="w-[30px] md:w-[50px]"
+                    src="/images/socials/email.png"
+                  />
+                </a>
               </div>
             </div>
           </div>
