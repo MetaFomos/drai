709172ee4df6d607191a010b83f48e880f98b8a8
MetaFomos <dev.metafomos@gmail.com>
2023-05-31 08:20:13 +0200

    add new advisor

--- src/section/Advisor.js
+++ src/section/Advisor.js
@@ -67,6 +67,67 @@ const Advisor = () => {
           </div>
         </div>
       </div>
+      <div className="items-center flex pb-10 md:pb-20" id="sectionadvisor">
+        <div className="n-container">
+          <h1 className="glaserstencil font-normal text-[40px] md:text-[60px] golden-font pt-5">
+            EDWARD. KIM
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-5">
+            <div>
+              <img className="" src="/images/edward.png" />
+              <div className="flex w-[100%] mt-[-7%]">
+                <a
+                  className="w-[7%]"
+                  href="https://instagram.com/herbertrsim"
+                  target="_blank"
+                >
+                  <img src="/images/socials/instagram.png" />
+                </a>
+                <a
+                  className="w-[7%] ml-[1%]"
+                  href="https://www.linkedin.com/in/edward-kim-1760a12/"
+                  target="_blank"
+                >
+                  <img src="/images/socials/linkedin.png" />
+                </a>
+                <a
+                  className="w-[7%] ml-[1%]"
+                  href="https://facebook.com/herbertrsim"
+                  target="_blank"
+                >
+                  <img src="/images/socials/facebook.png" />
+                </a>
+                <a
+                  className="w-[7%] ml-[1%]"
+                  href="http://www.twitter.com/herbertrsim"
+                  target="_blank"
+                >
+                  <img src="/images/socials/twitter.png" />
+                </a>
+              </div>
+            </div>
+            <div>
+              <ul className="text-white">
+                <li className="text-[18px] md:text-[20px] px-2 text-left leading-8 list-disc">
+                  Edward is an experienced banker and fund manager specializing
+                  in post-IPO funding and M&A in the Pan-Asian region.
+                </li>
+                <li className="text-[18px] md:text-[20px] px-2 text-left mt-5 leading-8 list-disc">
+                  He has a proven track record in private equities, venture
+                  capital, and structured funds of hedge funds (FoHF).
+                </li>
+                <li className="text-[18px] md:text-[20px] px-2 text-left mt-5 leading-8 list-disc">
+                  With over 20 years of expertise, Edward has demonstrated
+                  exceptional leadership and strategic investment skills.
+                </li>
+                <li className="text-[18px] md:text-[20px] px-2 text-left mt-5 leading-8 list-disc">
+                  He holds a BSc in Econometrics and an MSc in Economics.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
